47d051226e4fc411ba639032084683ed248090c5
QuadrupleTurbo <46331538+QuadrupleTurbo@users.noreply.github.com>
2024-05-26 01:24:31 +0100

    Create Record.cs

---
+++ Shared/Record.cs
@@ -0,0 +1,17 @@
+﻿using CitizenFX.Core;
+
+namespace RecM
+{
+    public class Record
+    {
+        public int Time { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Right { get; set; }
+        public Vector3 Forward { get; set; }
+        public Vector3 Velocity { get; set; }
+        public float SteeringAngle { get; set; }
+        public float Gas { get; set; }
+        public float Brake { get; set; }
+        public bool UseHandbrake { get; set; }
+    }
+}
